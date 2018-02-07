8e7c2c50e430601882bcc03dfc801ebdcafb5eaf
Nicolas Byl <nico@nicolas-byl.eu>
2018-02-07 16:09:16 +0100

    load properties from secret

---
+++ src/main/java/de/codecentric/nbyl/lift_and_openshift_ri/PropertyResource.java
@@ -0,0 +1,42 @@
+package de.codecentric.nbyl.lift_and_openshift_ri;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.io.*;
+import java.util.Properties;
+
+@Path("/")
+public class PropertyResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject getProperties() throws IOException {
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+
+        loadProperties()
+                .entrySet()
+                .stream()
+                .forEach(entry -> builder.add((String) entry.getKey(),
+                        (String) entry.getValue()));
+
+        return builder.build();
+    }
+
+    private Properties loadProperties() throws IOException {
+        String path = System.getenv("CONFIG_PATH");
+        if (path == null) {
+            path = "/etc/myconfig";
+        }
+
+        Properties properties = new Properties();
+        try (InputStream input = new FileInputStream(new File(path, "environment.properties"))) {
+            properties.load(input);
+        }
+        return properties;
+    }
+}
